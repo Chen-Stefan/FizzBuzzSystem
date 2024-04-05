b9c0116cce33ef7dd8e2d56245ace0300f2adf50
Stefan Chen <zchen158@my.bcit.ca>
2024-04-05 11:26:01 -0700

    comit

--- FizzBuzzMvc/obj/Debug/net8.0/FizzbuzzMvc.AssemblyInfo.cs
+++ FizzBuzzMvc/obj/Debug/net8.0/FizzbuzzMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FizzBuzzMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f973c696c163fd739573772848b995d76cb8c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FizzBuzzMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FizzBuzzMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
